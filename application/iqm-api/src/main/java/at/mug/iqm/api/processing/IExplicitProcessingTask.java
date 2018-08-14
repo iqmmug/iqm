d9fcfa164a4e245911b3ade0aa70cfba76975515
iqmmug <iqmmug@6db3f4fe-f06e-430b-ab1c-60b3195d6299>
2018-08-14 11:05:54 +0000

    - changed automatically current year from 2017 to 2018 for every file on project build.

--- application/iqm-api/src/main/java/at/mug/iqm/api/processing/IExplicitProcessingTask.java
+++ application/iqm-api/src/main/java/at/mug/iqm/api/processing/IExplicitProcessingTask.java
@@ -14,7 +14,7 @@ import at.mug.iqm.api.operator.IResult;
  * 
  * This file is part of IQM, hereinafter referred to as "this program".
  * %%
- * Copyright (C) 2009 - 2017 Helmut Ahammer, Philipp Kainz
+ * Copyright (C) 2009 - 2018 Helmut Ahammer, Philipp Kainz
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
