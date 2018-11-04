09a861f552ae3c6661433900dd8940c9835df41a
iqmmug <iqmmug@6db3f4fe-f06e-430b-ab1c-60b3195d6299>
2018-11-04 21:04:49 +0000

    - modified properties only.

--- application/iqm-api/src/main/java/at/mug/iqm/commons/util/osea4java/classification/RythmChecker.java
+++ application/iqm-api/src/main/java/at/mug/iqm/commons/util/osea4java/classification/RythmChecker.java
@@ -18,6 +18,34 @@
  */
 
 package at.mug.iqm.commons.util.osea4java.classification;
+
+/*-
+ * #%L
+ * Project: IQM - API
+ * File: RythmChecker.java
+ * 
+ * $Id$
+ * $HeadURL$
+ * 
+ * This file is part of IQM, hereinafter referred to as "this program".
+ * %%
+ * Copyright (C) 2009 - 2018 Helmut Ahammer, Philipp Kainz
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
 import static at.mug.iqm.commons.util.osea4java.classification.ECGCODES.*;
 import java.util.Arrays;
 
